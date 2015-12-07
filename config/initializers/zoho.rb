2d3b29319c4b2dafd08017790767794f27859895
Eric Stephenson <ericb.stephenson@gmail.com>
2015-12-07 12:51:39 -0800

    zoho initialized

--- config/initializers/zoho.rb
+++ config/initializers/zoho.rb
@@ -1,7 +1,7 @@
 require 'ruby_zoho'
 
 RubyZoho.configure do |config|
-  config.api_key = '' #put your api key between the quotes
+  config.api_key = '487cfb4daa516e269200855f9efc6b7d' #put your api key between the quotes
   # config.crm_modules = ['Accounts', 'Contacts', 'Leads', 'Potentials'] # Defaults to free edition if not set
   # config.crm_modules = ['Quotes', 'SalesOrders', 'Invoices'] # Depending on what kind of account you've got, adds to free edition modules
   # config.ignore_fields_with_bad_names = true # Ignores field with special characters in the name (Release 1.8)
