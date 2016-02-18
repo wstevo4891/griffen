d10e9956abacce935f6c76ff71ef707827fa0c4d
Eric Stephenson <ericb.stephenson@gmail.com>
2016-02-18 15:11:54 -0800

    more adjustments to admin products views

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 Product.delete_all
 Product.create!(title: 'Griffen Vx510',
   description: 
-    %{<p>
+    %{
         GRIFFEN is the first of its kind Credit Card POS System developed exclusively for 
         the legal marijuana industry. With patented double encrypted software and a FSC 
         code specific to marijuana together with reliable Verifone hardware, GRIFFEN 
@@ -12,13 +12,13 @@ Product.create!(title: 'Griffen Vx510',
         product for  a dollar payment, and securely processed through the Credit Card 
         Processing System with ACH Deposits to bank accounts in exactly the same manner 
         as any  other retailer in the USA.
-      </p>},
+      },
   image_url:   'vx510.jpg',    
   price: 650.00)
 # . . .
 Product.create!(title: 'Griffen Vx610',
   description:
-    %{<p>
+    %{
         GRIFFEN is the first of its kind Credit Card POS System developed exclusively for 
         the legal marijuana industry. With patented double encrypted software and a FSC 
         code specific to marijuana together with reliable Verifone hardware, GRIFFEN 
@@ -26,6 +26,6 @@ Product.create!(title: 'Griffen Vx610',
         product for  a dollar payment, and securely processed through the Credit Card 
         Processing System with ACH Deposits to bank accounts in exactly the same manner 
         as any  other retailer in the USA.
-      </p>},
+      },
   image_url: 'vx610.jpg',
   price: 650.00)
