c84918c4ec4d3aaae249aa63abd3b84388e1f6cb
Eric Stephenson <ericb.stephenson@gmail.com>
2016-04-09 19:12:51 -0700

    removed confirmable from users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :zxcvbnable
+         :recoverable, :rememberable, :validatable
 
   validates :firstname, :lastname, :business, :phone, presence: true
 
